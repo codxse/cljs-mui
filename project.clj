b93305b5db462fba6bb236b6f73c63b7c0d8f1c5
Nadiar AS <codxse@gmail.com>
2018-11-11 09:49:49 +0700

    remove react

--- project.clj
+++ project.clj
@@ -5,8 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.439"]
-                 [cljsjs/react "16.6.0-0"]
-                 [cljsjs/react-dom "16.6.0-0"]
                  [cljsjs/material-ui "3.2.0-0"]
                  [cljsjs/material-ui-icons "3.0.1-0"]
                  [fulcrologic/fulcro "2.6.15"]])
