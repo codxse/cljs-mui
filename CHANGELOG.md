e05e22a0c3ddea19410128a510446f5d912a1c22
Nadiar AS <codxse@gmail.com>
2018-12-22 15:49:44 +0700

    change assert with throw

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
-## "0.1.0-alpha4.2" - 2018-21-1
+## "0.1.0-alpha4.2" - 2018-12-22
+- Fixed bug with advanced compilation doesn't accept `assert`
+
+## "0.1.0-alpha4.2" - 2018-12-01
 - Fixed bug for multiple children/node not rendered.
 
 ## "0.1.0-alpha4.1" - 2018-11-30
--- src/id/nadiar/cljs_mui/style.cljs
+++ src/id/nadiar/cljs_mui/style.cljs
@@ -7,35 +7,42 @@
 
 (def createMuiTheme (gobj/get js/MaterialUI "createMuiTheme"))
 
+(def keyword-or-string? #(or (string? %) (keyword? %)))
+
 (defn color-for
   "Get the color from Material color db.
    Accept:
-   color as keyword
+   color as keyword / string
    level as integer
 
    Return `#000000` if color not found"
   [color_ level_]
-  (assert color_ "color must not be nil")
-  (assert level_ "color level must not be nil")
-  (get-in mui-colors [(name color_) (str level_)] "#000000"))
+  (if (keyword-or-string? color_)
+    (if (int? level_)
+      (get-in mui-colors [(name color_) (str level_)] "#000000")
+      (throw (js/Error. "`level_` must be integer.")))
+    (throw (js/Error. "`color_` must be string or keyword."))))
 
 (defn create-mui-theme
   "Like `createMuiTheme` material-ui, but accept clojure map."
   [raw-theme]
-  (assert (map? raw-theme) "theme must be valid clojure map.")
-  (createMuiTheme (clj->js raw-theme)))
+  (if (map? raw-theme)
+    (createMuiTheme (clj->js raw-theme))
+    (throw (js/Error. "`raw-theme` must be valid clojure map."))))
 
 (defn class-name
   "Function helper to get class-name from component.
    Accept react component and className as string or keyword. "
   [component id]
-  (assert (or (string? id) (keyword? id)) "`id` must be string or keyword.")
-  (gobj/getValueByKeys component "props" "classes" (name id)))
+  (if (keyword-or-string? id)
+    (gobj/getValueByKeys component "props" "classes" (clj->js id))
+    (throw (js/Error. "`id` must be string or keyword."))))
 
 (def theme (create-mui-theme {:typography {:useNextVariants true}}))
 
 (defn with-style
   "Return mui withStyle fn. Accept clojure map as argument."
   [m]
-  (assert (map? m) "`m` must be valid clojure map.")
-  (withStyles (clj->js m)))
+  (if (map? m)
+    (withStyles (clj->js m))
+    (throw (js/Error. "`m` must be valid clojure map."))))
